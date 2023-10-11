cc3931cadd180f0f4387b46b9ca601bf44baff1d
Daria Sapkina <daria.sapkina@mail.ru>
2023-10-11 21:12:49 +0300

    Записала основные методы, которые нужно реализовать

---
+++ src/main/java/Array/Array.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Array<E> {
+    public E[] arr;
+    private int capacity;
+    public int size;
+
+    public Array(int n){
+
+    }
+
+    public void add(E elem){
+
+    }
+
+    public E getElem(int i){
+        return null;
+    }
+
+    public void delete(E elem){
+
+    }
+}
